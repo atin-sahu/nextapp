9348aa4794c57a85a541f831531a23c73f011080
atin-sahu <jayshiv7171@gmail.com>
2023-08-24 00:26:20 +0530

    start

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      <h1 className='text-center'>Welcome Atin Sahu</h1>
+      <a href='/about'>go to about page</a>
+    </div>
+  )
+}
+
+export default index
